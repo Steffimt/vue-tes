97abdbb6200344eea6b5f6d16f0e7766f3828313
Steffimt <steffimariatanasia@gmail.com>
2024-02-02 22:58:47 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="header-container">
+      <h1 class="title">Weather App</h1>
+      <div class="position">
+        <div class="searchbar">
+          <input type="text" class="Input-form" placeholder="Enter a city">
+        </div>
+        <button class="btn">Search</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  body{
+    background-color: #121212 !important;
+  }
+  .header{
+    background-color: #212730;
+    border-radius: 20px;
+    color: #fff;
+    text-align: center;
+    font-family: arial, sans serif;
+  }
 }
+.header-container{
+    height: 25rem;
+    padding: 1.25rem 5rem;
+    .title{
+      margin-bottom: 2.5rem;
+    }
+    .position{
+      display: flex;
+      justify-content: center;
+      height: 25rem;
+    }
+    .searchbar{
+      margin-left: 0.5rem;
+      margin-right: 0.5rem;
+      width: 25rem;
+    }
+    .searchbar input {
+      box-sizing: border-box;
+      width: 100%;
+      padding: 0.5rem;
+      border-radius: 5px;
+      border: 1px solid black;
+    }
+    .btn{
+      height: 2rem;
+      background-color: #0000FF;
+      border-radius: 5px;
+      border: none;
+      padding: 0.5rem;
+      color: white;
+    }
+  }
+
 </style>
